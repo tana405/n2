e9dc2e60580fcc6614f10cf7a011303a5cc18455
Anna Trunova <trunova.am@yandex.ru>
2021-11-24 17:56:32 +0500

    asd

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import sys
 
-from PyQt5 import uic
 from random import randint
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPlainTextEdit, QPushButton, QLabel, QSpinBox, QCheckBox
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPlainTextEdit, QPushButton
 
 
-class FMain(QMainWindow):
-
+class Example(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('Ui.ui', self)
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(600, 600, 400, 400)
+        self.setWindowTitle('Random')
+        self.btn = QPushButton('Сгенерировать', self)
+        self.btn.setGeometry(300, 200, 30, 30)
         self.btn.clicked.connect(self.on_click)
 
     def on_click(self):
-        QMainWindow.setBrush(QColor(255, 255, 0))
+        QMainWindow.setBrush(QColor(randint(1, 255), randint(1, 255), randint(1, 255)))
         r = randint(1, 150)
         x = randint(1, 150)
         y = randint(1, 150)
@@ -23,6 +27,6 @@ class FMain(QMainWindow):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = FMain()
+    ex = Example()
     ex.show()
     sys.exit(app.exec_())
